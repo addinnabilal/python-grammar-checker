e9b7f4e4dac65e4a13d2db178ce06d87c20e0c4f
gagaspbahar <gagaspbahar@gmail.com>
2021-11-24 18:20:05 +0700

    v2 parser

--- src/parser.py
+++ src/parser.py
@@ -2,16 +2,8 @@ import os
 import argparse
 import converter_to_cnf
 import tokenizer
-import time, sys
-
-# def loading():
-#     animation = "|/-\\"
-
-#     for i in range(100):
-#         time.sleep(0.1)
-#         sys.stdout.write("\rParsing.." + animation[i % len(animation)])
-#         sys.stdout.flush()
-#         #do something
+import sys
+from time import time
 
 class Node:
     # Initialize the node with two child nodes, right node can be empty
@@ -81,6 +73,59 @@ class Parser:
                     sys.stdout.flush()
                     i += 1
 
+    def toDict(self, grammar):
+        ans = {}
+        for g in grammar:
+            ans[str(g[0])] = []
+
+        for g in grammar:
+            prod = []
+            for i in range(1,len(g)):
+                temp = g[i]
+                prod.append(temp)
+            ans[str(g[0])].append(prod)
+        return ans
+
+    def parsev2(self):
+        animation = "|/-\\"
+        n = len(self.input)
+        R = self.toDict(self.grammar)
+        print(self.input)
+        # Initialize the table
+        self.parsetable = [[set([]) for j in range(n)] for i in range(n)]
+
+        # Filling in the parsetable
+        for j in range(0, n):
+
+            # Check form S -> a
+            for left, rule in R.items():
+                for right in rule:
+                    # If a terminal is found
+                    if len(right) == 1 and right[0] == ("'" + self.input[j] + "'"):
+                        self.parsetable[j][j].add(left)
+
+
+            # Check form S -> AB
+            for i in range(j, -1, -1):      
+                for k in range(i, j):   
+                    # Iterate over rules
+                    for left, rule in R.items():
+                        for right in rule:
+                            # Checking for terminals
+                            if len(right) == 2 and right[0] in self.parsetable[i][k] and right[1] in self.parsetable[k + 1][j]:
+                                self.parsetable[i][j].add(left)
+                                
+                    sys.stdout.write("\rParsing.." + animation[k % len(animation)])
+                    sys.stdout.flush()
+        print()
+
+    def resultv2(self):
+        n = len(self.input)
+        if len(self.parsetable[0][n-1]) != 0:
+            print("Accepted.")
+        else:
+            print("Syntax error.")
+
     # Open grammar from file
     def grammarFromFile(self, g):
         cfg = converter_to_cnf.read_grammar(g)
@@ -105,7 +150,12 @@ if __name__ == "__main__":
     arg.add_argument("sentence")
 
     args = arg.parse_args()
+    start = time()
     TestParser = Parser(args.grammar, args.sentence)
-    TestParser.parse()
-    print()
-    TestParser.result()
+    TestParser.parsev2()
+    TestParser.resultv2()
+    # TestParser.parse()
+    # print()
+    # TestParser.result()
+    timeTaken = time()-start
+    print(f"Time taken: {timeTaken} s")
